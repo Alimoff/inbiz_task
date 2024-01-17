7e359ccee6f67b6064f0207ef93beb6c807035d9
Alimoff <sobiriddin@gmail.com>
2024-01-18 04:00:26 +0500

    created message route

---
+++ src/route/message.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { MessageController } from "../controller/message";
+
+export const messageRouter = Router();
+const controller = new MessageController();
+
+
+messageRouter.get("/message/:from/:to", controller.getMessages);
+messageRouter.post("/message", controller.createMessage);
