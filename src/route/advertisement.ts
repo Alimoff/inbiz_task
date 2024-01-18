c5704c2c7517e55fcdbb6ae58e57b2aefbd66312
Alimoff <sobiriddin@gmail.com>
2024-01-18 23:38:10 +0500

    add validation to advs

--- src/route/advertisement.ts
+++ src/route/advertisement.ts
@@ -2,14 +2,16 @@ import { Router } from 'express'
 import { AdvertisementController } from '../controller/advertisement';
 import { authenticateUser } from '../middleware/authenticate';
 import { uploadStorage } from '../middleware/multer';
+import { createAdValidationSchema, updateAdValidationSchema } from '../validation/schemas/advertisement';
+import {validate} from "../middleware/validate";
 
 const advRouter = Router()
 const controller = new AdvertisementController()
 
 advRouter.get('/adv', controller.getAll, );
 advRouter.get('/adv/:id', controller.getById);
-advRouter.post('/adv', uploadStorage.single('file'),authenticateUser,controller.create);
-advRouter.put('/adv/:id',uploadStorage.single('file'),authenticateUser, controller.update);
+advRouter.post('/adv', validate(createAdValidationSchema), uploadStorage.single('file'),authenticateUser,controller.create);
+advRouter.put('/adv/:id', validate(updateAdValidationSchema), uploadStorage.single('file'),authenticateUser, controller.update);
 advRouter.delete('/adv/:id',authenticateUser, controller.delete);
 
 export { advRouter }
