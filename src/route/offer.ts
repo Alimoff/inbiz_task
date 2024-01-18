6996188497e0898638d266ec9b614f80f1bc8e72
Alimoff <sobiriddin@gmail.com>
2024-01-18 23:37:34 +0500

    validation for offer

--- src/route/offer.ts
+++ src/route/offer.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express'
 import { OfferController } from '../controller/offer';
 import {requireAdmin, validate} from "../middleware/validate";
+import { createOfferValidationSchema, updateOfferValidationSchema } from '../validation/schemas/offer';
 
 const offerRouter = Router()
 const controller = new OfferController()
 
 offerRouter.get('/offer', controller.getAll, );
 offerRouter.get('/offer/:id', controller.getById);
-offerRouter.post('/offer', controller.create);
-offerRouter.put('/offer/:id', controller.update);
+offerRouter.post('/offer', validate(createOfferValidationSchema), controller.create);
+offerRouter.put('/offer/:id', validate(updateOfferValidationSchema), controller.update);
 offerRouter.delete('/offer/:id', controller.delete);
 
 
