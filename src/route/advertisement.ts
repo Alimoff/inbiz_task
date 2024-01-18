ade32fe19e04a5f69e3d013ba058624d78500c69
Alimoff <sobiriddin@gmail.com>
2024-01-18 21:31:06 +0500

    add auths in routes

--- src/route/advertisement.ts
+++ src/route/advertisement.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express'
 import { AdvertisementController } from '../controller/advertisement';
-import {requireAdmin, validate} from "../middleware/validate";
 import { authenticateUser } from '../middleware/authenticate';
+import { uploadStorage } from '../middleware/multer';
 
 const advRouter = Router()
 const controller = new AdvertisementController()
 
 advRouter.get('/adv', controller.getAll, );
 advRouter.get('/adv/:id', controller.getById);
-advRouter.post('/adv', authenticateUser,controller.create);
-advRouter.put('/adv/:id', controller.update);
-advRouter.delete('/adv/:id', controller.delete);
+advRouter.post('/adv', uploadStorage.single('file'),authenticateUser,controller.create);
+advRouter.put('/adv/:id',uploadStorage.single('file'),authenticateUser, controller.update);
+advRouter.delete('/adv/:id',authenticateUser, controller.delete);
 
 export { advRouter }
