e8b4cb05633a92ebb0c551d9c78233d2549d3531
Alimoff <sobiriddin@gmail.com>
2024-01-18 21:28:56 +0500

    fix:multer

--- src/controller/advertisement.ts
+++ src/controller/advertisement.ts
@@ -5,6 +5,7 @@ import { Category } from '../types/common';
 import dotenv from 'dotenv';
 import { autoDeleteExpiredAds } from '../middleware/autoDelete';
 import jwt, { decode } from 'jsonwebtoken';
+import { authenticateUser } from '../middleware/authenticate';
 
 dotenv.config()
 
@@ -48,7 +49,13 @@ export class AdvertisementController {
     // To create new advertisement
     public async create(req: Request, res: Response, next: NextFunction){
         try{
-            const {title, description, category,price, image,duration} = req.body;
+            const {title, description, category,price,duration} = req.body; 
+            //Using multer to upload file. It takes path of file and saves current file to folder
+            //The reason of saving file path to db is to provide upload faster
+            let file = '';
+            if(req.file) file = `${req.file.filename}`
+            console.log(req.file)
+
             const publishedDate = Date.now();
             const expirationDate = new Date();
             expirationDate.setDate(expirationDate.getDate() + duration);
@@ -56,20 +63,17 @@ export class AdvertisementController {
             //To get accessToken if user is authorized
             const authorizationHeader = req.headers.authorization;
 
-    
             if (!authorizationHeader) {
                 // Handle case where no access token is present
-                return res.status(401).json({ error: 'Unauthorized - Missing token' });
-              }
-    
-            const token = authorizationHeader.split(' ')[1];
+                return res.status(401).json({ error: 'Unauthorized - Missing token' });}
 
+            const token = authorizationHeader.split(' ')[1];
               //Get user._id from accessToken
             const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as { id: string };
             const publishedBy = decodedToken.id;
 
             const newAdvertisement = new AdvertisementModel({
-                title, description, category, price, image,duration,publishedDate,expirationDate, archived:false,publishedBy
+                title, description, category, price, file,duration,publishedDate,expirationDate, archived:false,publishedBy
             });
             await newAdvertisement.save();
 
@@ -82,19 +86,20 @@ export class AdvertisementController {
     //Method PUT
     //To update an advertisement by ID
     public async update(req: Request, res: Response, next: NextFunction){
-        const {title, description, category,price, image} = req.body;
+        const {title, description, category,price, file} = req.body;
         const {id} = req.params;
 
         try{
             const updatedAdvertisement = await AdvertisementModel.findOneAndUpdate({_id:id},{
-                title, description, category, price, image});
+                title, description, category, price, file});
+           
 
             if (![Category.CARS, Category.CLOTHES, Category.COSMETICS, Category.ELECTRONICS, Category.FOOD_AND_BEVERAGE,
                 Category.FURNITURE_AND_DECOR, Category.HEALTH, Category.HOUSEHOLD_ITEMS, Category.OFFICE_EQUIPMENT, Category.OTHER].includes(category)){
                     return res.status(StatusCodes.BAD_REQUEST).json({error: `There is no ${category} category`});
                   }
 
-            updatedAdvertisement?.save();
+            await updatedAdvertisement?.save();
 
             return res.status(StatusCodes.OK).json({message: updatedAdvertisement});
 
@@ -109,7 +114,8 @@ export class AdvertisementController {
         const {id} = req.params;
         try{
             const deleteAdvertisement = await AdvertisementModel.findOneAndDelete({_id:id});
-            deleteAdvertisement?.save();
+            await deleteAdvertisement?.save();
+
             res.status(StatusCodes.OK).json({message: "Advertisement deleted successfully!"});
         }catch(error){
             return res.status(StatusCodes.BAD_REQUEST).json({message: error});
