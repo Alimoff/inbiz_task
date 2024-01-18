5246209b12e9fb7be5e1707d6858837e239bb01b
Alimoff <sobiriddin@gmail.com>
2024-01-18 23:40:42 +0500

    auth to message

--- src/route/message.ts
+++ src/route/message.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { MessageController } from "../controller/message";
+import { authenticateUser } from "../middleware/authenticate";
 
 export const messageRouter = Router();
 const controller = new MessageController();
 
-messageRouter.get("/message/:from/:to", controller.getMessages);
-messageRouter.post("/message", controller.createMessage);
+messageRouter.get("/message/:from/:to",authenticateUser, controller.getMessages);
+messageRouter.post("/message",authenticateUser, controller.createMessage);
