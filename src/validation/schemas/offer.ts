408ca7114add4bffd0a23a0532dc7268757694c1
Alimoff <sobiriddin@gmail.com>
2024-01-18 23:41:31 +0500

    validations for offer

---
+++ src/validation/schemas/offer.ts
@@ -0,0 +1,15 @@
+import yup from 'yup';
+
+export const createOfferValidationSchema = yup.object({
+    adId: yup.string().required(),
+    userId: yup.string().required(),
+    price: yup.number(),
+    description: yup.string(),
+    date: yup.date().required(),
+})
+
+export const updateOfferValidationSchema = yup.object({
+    price: yup.number(),
+    description: yup.string(),
+    date: yup.date().required(),
+})
